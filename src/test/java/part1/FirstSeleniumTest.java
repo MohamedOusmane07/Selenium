9d252605a8f321a72f6d620c6bc03cb9273fa36f
Ousmane Ibrahim Mohamed <ousibmohamed@gmail.com>
2024-10-12 19:00:16 +0100

    FistSelenium Test

---
+++ src/test/java/part1/FirstSeleniumTest.java
@@ -0,0 +1,70 @@
+package part1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.safari.SafariDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import org.testng.asserts.Assertion;
+
+public class FirstSeleniumTest {
+
+    WebDriver driver; // The purpose of webDriver is to controle the browser and help to find element
+
+    @BeforeClass
+    void beforeClass() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+    }
+
+    @AfterClass
+    void afterClass() {
+        //driver.quit();
+    }
+
+
+    @Test
+    void testLoginIntoApplication() throws InterruptedException {
+        Thread.sleep(2000);
+        WebElement username = driver.findElement(By.name("username"));
+        username.sendKeys("Admin");
+         var password = driver.findElement(By.name("password"));
+         password.sendKeys("admin123");
+         driver.findElement(By.tagName("button")).click();
+         Thread.sleep(2000);
+
+         String actualResult = driver.findElement(By.tagName("h6")).getText();
+         String expectedResult = "Dashboard";
+         Assert.assertEquals(actualResult, expectedResult);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
